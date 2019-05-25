4dd019d52ac7148bb8804918c0131296576fc452
William Pescherine <wkpescherine@gmail.com>
2019-05-25 02:24:57 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,21 +1,21 @@
 def create_an_empty_array
-  
+  create_an_empty_array= []
 end
 
 def create_an_array
-  
+  create_an_array = [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop(element)
 end
 
 def remove_element_from_start_of_array(array)
